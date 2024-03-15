b12b89f7ea07545f4dbceca816718543c0a45c2f
iloveomelette <mizuki1998hosei@gmail.com>
2024-03-16 06:31:58 +0900

    Initial Commit

---
+++ reminder-list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reminder-list
+//
+//  Created by BC-PC148 on 2024/03/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
