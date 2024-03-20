004795036d224e3c849a3b977b883cdd51744fc1
iloveomelette <mizuki1998hosei@gmail.com>
2024-03-20 18:26:42 +0900

    :white_check_mark: Section6: Make the action accessible

--- reminder-list/ListViewController/ReminderListViewController+DataSource.swift
+++ reminder-list/ListViewController/ReminderListViewController+DataSource.swift
@@ -8,6 +8,14 @@ extension ReminderListViewController {
   typealias DataSource = UICollectionViewDiffableDataSource<Int, Reminder.ID>
   typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Reminder.ID>
   
+  var reminderCompletedValue: String {
+    NSLocalizedString("Completed", comment: "Reminder completed value")
+  }
+  
+  var reminderNotCompletedValue: String {
+    NSLocalizedString("Not completed", comment: "Reminder not completed value")
+  }
+  
   func updateSnapshot(reloading ids: [Reminder.ID] = []) {
     var snapshot = Snapshot()
     snapshot.appendSections([0])
@@ -40,6 +48,8 @@ extension ReminderListViewController {
 
     var doneButtonConfiguration = doneButtonConfiguration(for: reminder)
     doneButtonConfiguration.tintColor = .todayListCellDoneButtonTint
+    cell.accessibilityCustomActions = [doneButtonAccessibilityAction(for: reminder)]
+    cell.accessibilityValue = reminder.isComplete ? reminderCompletedValue : reminderNotCompletedValue
     cell.accessories = [
       .customView(configuration: doneButtonConfiguration),
       .disclosureIndicator(displayed: .always)
@@ -83,5 +93,18 @@ extension ReminderListViewController {
     reminder.isComplete.toggle()
     updateReminder(reminder)
     updateSnapshot(reloading: [id])
+  }
+  
+  private func doneButtonAccessibilityAction(for reminder: Reminder) -> UIAccessibilityCustomAction {
+    let name = NSLocalizedString("Toggle completion", comment: "Reminder done button accessibility label")
+    /*
+     * By default, closures create a strong reference to external values that you use inside them.
+     * Specifying a weak reference to the view controller prevents a retain cycle.
+     */
+    let action = UIAccessibilityCustomAction(name: name) { [weak self] action in
+      self?.completeReminder(withId: reminder.id)
+      return true
+    }
+    return action
   }
 }
